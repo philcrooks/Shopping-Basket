ec6e2709e39d69a24ad3416ccf008b740a6fdbd2
Phil Crooks <pacman@crooks.me.uk>
2016-09-14 08:47:36 +0100

    Added some explanation of my approach

--- specs/TestShoppingBasket.java
+++ specs/TestShoppingBasket.java
@@ -39,12 +39,14 @@ public class TestShoppingBasket {
 
   @Test
   public void testAddItem() {
+    // A customer clicks "buy now" on an item on the website
     basket.addItem(product1);
     assertEquals(5.0, basket.getPrice(), 0.001);
   }
 
   @Test
   public void testBogof() {
+    // A customer clicks "buy now" twice on an item that has a bogof offer
     basket.addItem(product1);
     basket.addItem(product1);
     assertEquals(5.0, basket.getPrice(), 0.001);
@@ -52,6 +54,8 @@ public class TestShoppingBasket {
 
   @Test
   public void testBasketDiscount_NoDiscount() {
+    // A customer clicks "buy now" twice on two items that have no special offers
+    // Note that the 10% discount does not come into effect until more than £20 is spent
     basket.addItem(product1);
     basket.addItem(product2);
     assertEquals(20.0, basket.getPrice(), 0.001);
@@ -59,6 +63,8 @@ public class TestShoppingBasket {
 
   @Test
   public void testBasketDiscount_BogofNoDiscount() {
+    // A customer buys two of one item and one of another. A bogof offer comes into effect.
+    // There is no "basket discount" because it does not come into effect until *more than* £20 is spent.
     basket.addItem(product1);
     basket.addItem(product1);
     basket.addItem(product2);
@@ -67,6 +73,8 @@ public class TestShoppingBasket {
 
   @Test
   public void testBasketDiscount_BogofWithDiscount() {
+    // A customer buys three of one item and one of another. A bogof offer comes into effect on one of the items.
+    // There is a "basket discount" of 10% because more than £20 is spent (£25 is spent).
     basket.addItem(product1);
     basket.addItem(product1);
     basket.addItem(product1);
@@ -76,6 +84,8 @@ public class TestShoppingBasket {
 
   @Test
   public void testBasketDiscount_NoBogofWithDiscount() {
+    // There is a "basket discount" of 10% because more than £20 is spent (£30 is spent).
+    // There is no bogof offer
     basket.addItem(product2);
     basket.addItem(product2);
     assertEquals(27.0, basket.getPrice(), 0.001);
@@ -83,14 +93,20 @@ public class TestShoppingBasket {
 
   @Test
   public void testBasketAndCustomerDiscount() {
+    // There is a "basket discount" of 10% because more than £20 is spent (£30 is spent).
+    // THe customer has a loyalty card so there is a further discount of 2% applied after the basket discount.
     basket.addItem(product2);
     basket.addItem(product2);
+    // Adding the customer to the basket after buying the items is the equivalent of logging into the system after shopping has started.
     basket.addCustomer(customer);
     assertEquals(26.46, basket.getPrice(), 0.001);
   }
 
   @Test
   public void testGetIndex() {
+    // Using the index of a line item to update the basket is the equivalent of interactining with the basket itself
+    // (rather than clicking on "buy now" elsewhere on the website).
+    // Check that the index can be found.
     basket.addItem(product2);
     basket.addItem(product1);
     assertEquals(0, basket.getIndex(product2));
@@ -99,15 +115,19 @@ public class TestShoppingBasket {
 
   @Test
   public void testRemoveByIndex() {
+    // Test that the user can reduce the number of items he wants by one
+    // Because there is only one item on the line, removing the item removes the line
     basket.addItem(product2);
     basket.addItem(product1);
     basket.removeItem(basket.getIndex(product2));
     assertEquals(5.0, basket.getPrice(), 0.001);
-    assertEquals(1, basket.size());
+    assertEquals(1, basket.size()); // Only one line item left
   }
 
   @Test
   public void testAddByIndex() {
+    // Test that the user can add one to the number of items he wants
+    // (by interacting with the basket rather than clicking on "buy now")
     basket.addItem(product2);
     basket.addItem(product1);
     basket.addItem(basket.getIndex(product2));
@@ -117,6 +137,8 @@ public class TestShoppingBasket {
 
   @Test
   public void testSetNumberOfItems() {
+    // Test that the user can arbitrarily change the number of items he wants
+    // (by interacting with the basket rather than clicking on "buy now")
     basket.addItem(product1);
     basket.setNumberOfItems(basket.getIndex(product1), 10);
     assertEquals(22.5, basket.getPrice(), 0.001);
@@ -125,9 +147,11 @@ public class TestShoppingBasket {
 
   @Test
   public void testClearBasket() {
+    // Test that the user can empty the basket
     basket.addItem(product1);
     basket.setNumberOfItems(basket.getIndex(product1), 10);
     basket.clearBasket();
+    // No line items and no price
     assertEquals(0.0, basket.getPrice(), 0.001);
     assertEquals(0, basket.size());
   }
